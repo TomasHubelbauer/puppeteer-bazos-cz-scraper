c6a30334b36eabb7fca645142a400b4922e46f44
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-07-13 10:32:09 +0200

    Update deps, clean up and add a demo

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Bazos.cz scraper built using Puppeteer used for obtaining search results as JSON
 
 ## Running
 
-```sh
-cd src
-nvm use
-yarn
-node --experimental-modules index.mjs "query" zip priceMin priceMax # Or `nodemon` for continuous file change watching.
-```
+- `npm run demo` for an iPhone in Prague 1 between 3000 and 6000 CZK demo query
+
+![](demo.gif)
+
+- `npm start -- search {query} {zip} -f {priceFrom} -t {priceTo}` for custom query (headless)
+- `npm start -- search {query} {zip} -f {priceFrom} -t {priceTo} -w` for custom query (non-headless)
+- `npm start -- -h` for the program help
+- `npm start -- search -h` for the `search` command help
 
 ## To-Do
 
@@ -17,12 +19,10 @@ Also see `TODO` comments in the code.
 
 Open detail for each post and fetch full description from it.
 
-Persist captured errors to a log file.
-
 Implement diff to report updates/inserts in a separate file.
-
-Add a guide on how to hook up with cron for email reporting.
 
 Run this in Azure Pipelines using a scheduled trigger and
 push the generated report back to the repo and set up GitHub
 Pages for the repository to show it on a live URL.
+
+Consider having the pipeline send out an email with a diff.
---
+++ index.mjs
@@ -0,0 +1,130 @@
+import puppeteer from 'puppeteer';
+import commander from 'commander';
+import filenamify from 'filenamify';
+import fs from 'fs-extra';
+import metadata from './package.json';
+
+commander
+  .version(metadata.version)
+  .command('search [query] [zip]')
+  .description('Searches Bazos.cz using the given search query within the given Czech Republic ZIP code area.')
+  .option('-f, --from [amount]', 'price from amount (CZK)')
+  .option('-t, --to [amount]', 'price to amount (CZK)')
+  .option('-w, --window', 'to run in non-headless mode (windowed)')
+  .action(scrape)
+  ;
+
+commander.parse(process.argv);
+
+async function scrape(query, zip, { from: priceMin, to: priceMax, window }) {
+  console.log(`Searching for '${query}' in Czech Republic ZIP code area ${zip} priced between ${priceMin} and ${priceMax} CZK.`);
+  const browser = await puppeteer.launch({ headless: !window, slowMo: 10 });
+  const page = (await browser.pages())[0];
+  await page.bringToFront();
+  await page.goto('https://bazos.cz');
+
+  try {
+    const hledatInput = await page.$('#hledat');
+    await hledatInput.focus();
+    await page.keyboard.type(query);
+
+    // Discard the autocomplete prompt.
+    if (window) {
+      await page.waitForSelector('#vysledek');
+      await page.evaluate(() => document.getElementById('vysledek').remove());
+    }
+
+    const hlokalitaInput = await page.$('#hlokalita');
+    await hlokalitaInput.focus();
+    await page.keyboard.type(zip);
+
+    // Discard the autocomplete prompt.
+    if (window) {
+      await page.waitForSelector('#vysledekpsc');
+      await page.evaluate(() => document.getElementById('vysledekpsc').remove());
+    }
+
+    if (priceMin) {
+      const cenaodInput = await page.$('input[name=cenaod]');
+      await cenaodInput.focus();
+      await page.keyboard.type(priceMin);
+    }
+
+    if (priceMax) {
+      const cenadoInput = await page.$('input[name=cenado]');
+      await cenadoInput.focus();
+      await page.keyboard.type(priceMax);
+    }
+
+    const submitInput = await page.$('input[name=Submit]');
+    await submitInput.click();
+
+    await page.waitForNavigation();
+
+    const start = new Date();
+
+    let total;
+    const results = [];
+    let hasNextPage = false;
+    do {
+      const summaryText = await page.$eval('table.listainzerat > tbody > tr > td', listaTd => listaTd.textContent);
+      const summaryTextParts = summaryText.trim().split(/[\s-]/g);
+      const firstPostNo = Number(summaryTextParts[1]);
+      const lastPostNo = Number(summaryTextParts[2]);
+      const pageTotal = lastPostNo - firstPostNo + 1;
+      total = Number(summaryTextParts[5]);
+
+      console.log(`Showing posts #${firstPostNo}-${lastPostNo} (${pageTotal} on the page) out of ${total} total results.`);
+
+      const pageResults = await page.evaluate(pageTotal => {
+        let results = [];
+
+        const vypisSpans = document.querySelectorAll('span.vypis');
+        for (let index = 0; index < vypisSpans.length; index++) {
+          const vypisSpan = vypisSpans[index];
+
+          const nadpisA = vypisSpan.querySelector('span.nadpis > a');
+          const velikostSpan = vypisSpan.querySelector('span.velikost10');
+          const popisDiv = vypisSpan.querySelector('div.popis');
+          const cenaSpan = vypisSpan.querySelector('span.cena > b');
+
+          const title = nadpisA.textContent;
+          const url = nadpisA.href;
+          const [year, month, day] = velikostSpan.lastChild.textContent.substr(4).slice(0, -1).split('.').map(n => Number(n.trim())).reverse();
+          const description = popisDiv.textContent;
+          const price = Number(cenaSpan.textContent.slice(2, -3).replace(/ /, ''));
+
+          results.push({ title, url, year, month, day, description, price });
+        }
+
+        if (results.length !== pageTotal) {
+          throw new Error(`Expected to collect ${pageTotal} posts on the page but got ${results.length}.`);
+        }
+
+        const strankovaniA = document.querySelector('p.strankovani > a:last-child');
+        return { results, nextPageHref: strankovaniA && strankovaniA.href };
+      }, pageTotal);
+
+      results.push(...pageResults.results);
+
+      if (pageResults.nextPageHref) {
+        await page.goto(pageResults.nextPageHref);
+        hasNextPage = true;
+        console.log(`Collected ${pageResults.results.length} results on the page, ${results.length} total so far.\nAdvancing to the further page.`);
+      } else {
+        hasNextPage = false;
+        console.log(`Collected ${pageResults.results.length} results on the page, ${results.length} total.\nQuitting as this is the final page.`);
+      }
+    } while (hasNextPage);
+
+    if (results.length !== total) {
+      throw new Error(`Expected to collect ${total} posts but got ${results.length}.`);
+    }
+
+    const end = new Date();
+
+    await fs.writeJSON(filenamify(`${query}-in-${zip}-from-${priceMin || 'any'}-czk-to-${priceMax || 'any'}-czk.json`), { start, end, results }, { spaces: 2 });
+  } finally {
+    await browser.close();
+  }
+}
